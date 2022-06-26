e8e2ba833ca6733524592599f0a6572950fa5ad2
Shyne <yeshai007@gmail.com>
2022-06-26 23:14:56 +0800

    Create bq-api.js

---
+++ bq-api.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// use helmet
+const helmet = require('helmet')
+app.use(helmet())
+
+// use body-parser = express
+app.use(express.json())
+app.use(express.urlencoded({ extended: true, }))
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// serve
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
