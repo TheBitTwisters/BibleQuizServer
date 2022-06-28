68ecfd925f967a0287f7f44e661bfb8c60e3133b
Michael Angelo <michael01angelo@gmail.com>
2022-06-28 10:58:44 +0800

    Create BaseModel.js

---
+++ models/BaseModel.js
@@ -0,0 +1,121 @@
+const mysql = require('../util/mysql')
+
+const BaseModel = class BaseModel {
+  static get tableName() {
+    return ''
+  }
+  static get sortBy() {
+    return {
+      'id': 'DESC'
+    }
+  }
+
+  id = 0 // int
+
+  static search(whereParams, likeParams) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        var q = new mysql.SearchQuery()
+        await q.select('*').from(this.tableName).where(whereParams).like(likeParams).sortBy(this.sortBy).execute()
+        var results = q.getList()
+        var list = []
+        for (var result of results) {
+          list.push(new this(result))
+        }
+        resolve(list)
+      } catch (err) {
+        console.error(err)
+        reject({
+          err: true,
+          code: 503,
+          message: 'Internal(DB) server error'
+        })
+      }
+    })
+  }
+
+  static getAll() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        var q = new mysql.Query()
+        await q.select('*').from(this.tableName).sortBy(this.sortBy).execute()
+        var results = q.getList()
+        var list = []
+        for (var result of results) {
+          list.push(new this(result))
+        }
+        resolve(list)
+      } catch (err) {
+        console.error(err)
+        reject({
+          err: true,
+          code: 503,
+          message: 'Internal(DB) server error'
+        })
+      }
+    })
+  }
+
+  static get(params) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        var q = new mysql.Query()
+        await q.select('*').from(this.tableName).where(params).execute()
+        var result = q.getOne()
+        if (result) {
+          resolve(new this(result))
+        }
+        resolve(false)
+      } catch (err) {
+        console.error(err)
+        reject({
+          err: true,
+          code: 503,
+          message: 'Internal(DB) server error'
+        })
+      }
+    })
+  }
+
+  constructor(param = {}) {
+    this.id = param.id || 0
+  }
+
+  save() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        var jsonData = this.toJsonData()
+        if (Object.keys(jsonData).length > 0) {
+          if (this.id > 0) {
+            var u = new mysql.Update()
+            await u.update(this.constructor.tableName).set(jsonData).where({ id: this.id }).execute()
+            if (u.result) {
+              resolve(true)
+            }
+          } else {
+            var i = new mysql.Insert()
+            await i.insertInto(this.constructor.tableName).values(jsonData).execute()
+            if (i.insertId > 0) {
+              this.id = i.insertId
+              resolve(true)
+            }
+          }
+        }
+        resolve(false)
+      } catch (err) {
+        console.error(err)
+        reject({
+          err: true,
+          code: 503,
+          message: 'Internal(DB) server error'
+        })
+      }
+    })
+  }
+
+  toJsonData() {
+    return {}
+  }
+}
+
+module.exports = BaseModel
