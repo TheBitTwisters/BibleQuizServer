a26ad027853c0265b2c9961e65e5dfc06d067dfe
Michael Angelo <michael01angelo@gmail.com>
2022-07-01 16:33:40 +0800

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -8,6 +8,7 @@ const Player = class Player extends BaseModel {
   id        = 0          // int
   active    = true       // tinyint(1)
   name      = ""         // varchar(64)
+  fullname  = ""         // varchar(255)
   joined_at = Date.now() // datetime : default now()
 
   constructor(param = {}) {
@@ -15,6 +16,7 @@ const Player = class Player extends BaseModel {
     this.id        = param.id        || 0
     this.active    = param.active    || 1
     this.name      = param.name      || ""
+    this.fullname  = param.fullname  || ""
     this.joined_at = param.joined_at || Date.now()
   }
 
@@ -23,14 +25,16 @@ const Player = class Player extends BaseModel {
       id:        this.id,
       active:    this.active,
       name:      this.name,
+      fullname:  this.fullname,
       joined_at: this.joined_at
     }
   }
 
   toJsonData() {
     return {
-      active: this.active,
-      name:   this.name
+      active:   this.active,
+      name:     this.name,
+      fullname: this.fullname
     }
   }
 }
